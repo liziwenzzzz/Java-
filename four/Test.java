8475b5529fd67520d7a1239fe4f7199c256d4bad
liziwen <1020132719@qq.com>
2017-11-25 10:52:38 +0800

    comimt

--- four/Test.java
+++ four/Test.java
@@ -9,21 +9,27 @@ public class Test {
 		String path = new Scanner(System.in).nextLine();
 		Helper it = new Helper();
 		it.getFileContent(path);
-		System.out.println("1.get count of letter\n2.get count of word");
+
 		int flag = 0;
 		Scanner input = new Scanner(System.in);
-		flag = input.nextInt();
-		switch (flag) {
-		case 1:
-			it.countLetter();
-			it.showLetterCount();
-			break;
-		case 2:
-			it.countWord();
-			it.showWordCount();
-			break;
-		default:
-			System.out.println("wrong");
+
+		while (true) {
+			System.out.println("1.get count of letter\n2.get count of word");
+			flag = input.nextInt();
+			switch (flag) {
+			case 1:
+				it.countLetter();
+				it.showLetterCount();
+				break;
+			case 2:
+				it.countWord();
+				it.showWordCount();
+				break;
+			default:
+				System.out.println("input wrong,exit!");
+				return;
+			}
 		}
+
 	}
 }
